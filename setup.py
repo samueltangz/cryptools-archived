a40e77d39f09c44683107ebc786e5a6a3ca8588a
Samuel Tang <samueltangz613@gmail.com>
2018-10-27 23:59:24 +0800

    Added dependencies back

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='cryptools',
-    version='0.0.1',
-    description='A Python module with common cryptographic attacks',
-    author='0x000505AD',
-    url='https://github.com/samueltangz/cryptools',
-    packages=find_packages(),
-    test_suite='test.suite',
-    install_requires=[]
+  version='0.0.1',
+  description='A Python module with common cryptographic attacks',
+  author='0x000505AD',
+  url='https://github.com/samueltangz/cryptools',
+  packages=find_packages(),
+  test_suite='test.suite',
+  install_requires=[
+    'gmpy2',
+    'pycrypto'
+  ]
 )
